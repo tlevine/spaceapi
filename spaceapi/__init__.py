53ec85f3e934da2de5327050c96ab7fb4a576e37
Thomas Levine <_@thomaslevine.com>
2015-05-13 08:51:51 +0000

    -

--- spaceapi/__init__.py
+++ spaceapi/__init__.py
@@ -1,13 +1,13 @@
-import datetime, sys, csv
+import datetime, sys
 from concurrent.futures import ThreadPoolExecutor
 
 from . import download
 
-def download_all():
+def download_all(resolution = 5 * 60, threads = 30):
     # Download
     timestamp = int(datetime.datetime.now().timestamp())
-    minute = timestamp - (timestamp % (5 * 60))
-    with ThreadPoolExecutor(30) as e:
+    minute = timestamp - (timestamp % (resolution))
+    with ThreadPoolExecutor(threads) as e:
         spaces = download.directory(minute).json().values()
         futures = [(space, e.submit(download.space, minute, space)) for space in spaces]
     for space, future in futures:
@@ -15,6 +15,7 @@ def download_all():
             sys.stderr.write('Exception downloading %s:\n%s\n\n' % (space, future.exception()))
 
 def emit():
+    import csv
     w = csv.writer(sys.stdout)
     w.writerow(('space', 'timestamp', 'open'))
     for minute in download.directory.keys():
@@ -32,5 +33,20 @@ def emit():
             w.writerow((space, minute, open_str))
 
 def main():
-    download()
-    emit()
+    import argparse
+    p = argparse.ArgumentParser(description = 'Download Space API data.')
+    p.add_argument('-d', '--download', help = 'Download new data?',
+                   action = 'store_true')
+    p.add_argument('-e', '--csv', help = 'Emit data as CSV?',
+                   action = 'store_true')
+    args = p.parse_args()
+
+    if args.download:
+        download()
+
+    if args.emit:
+        emit()
+
+    if not args.download and not args.emit:
+        sys.stderr.write('Doing nothing; try running with the --help flag.\n')
+        sys.exit(1)
