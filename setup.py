fee8d3fbfa24cd43ecb99776bb0b1d918a98ff9b
Thomas Levine <_@thomaslevine.com>
2015-05-15 20:12:11 +0000

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'spaceapi',
-    version = '0.1.2',
+    version = '0.1.3',
     description = 'Download and archive Space API data',
     author = 'Thomas Levine',
     author_email = '_@thomaslevine.com',
