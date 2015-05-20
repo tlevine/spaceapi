1de024ad55e9f3f4a296b904c4febec12010904c
Thomas Levine <_@thomaslevine.com>
2015-05-20 20:03:41 +0000

    immutable

--- spaceapi/download.py
+++ spaceapi/download.py
@@ -12,11 +12,11 @@ class int_transformer:
         assert len(path) == 1, path
         return int(path[0])
 
-@vlermv.cache(DIR, 'directory', key_transformer = int_transformer)
+@vlermv.cache(DIR, 'directory', key_transformer = int_transformer, mutable = False)
 def directory(timestamp):
     url = 'http://spaceapi.net/directory.json'
     return requests.get(url, headers = HEADERS)
 
-@vlermv.cache(DIR, 'space')
+@vlermv.cache(DIR, 'space', mutable = False)
 def space(datetime, url):
     return requests.get(url, verify = False, headers = HEADERS)
