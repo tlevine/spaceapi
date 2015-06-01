6eae3a71c9a5b8245a24791464f2ef58245e6818
Thomas Levine <_@thomaslevine.com>
2015-06-01 14:22:41 +0000

    -

--- spaceapi/__init__.py
+++ spaceapi/__init__.py
@@ -14,9 +14,19 @@ def get(resolution = 5 * 60, threads = 30):
         if future.exception() is not None:
             sys.stderr.write('Exception downloading %s:\n%s\n\n' % (url, future.exception()))
 
-def read():
+ONE_WEEK = 7 * 24 * 3600
+def read(annotate = False, start = None):
+    '''
+    Read the data.
+    
+    :param bool annotate: Include extra information useful for plotting.
+    :param datetime start: Read only the readings from a particular date on.
+    '''
     spaces = master.directory()
     for minute in download.directory.keys():
+        if start and minute < start:
+            continue
+
         for space, url in spaces.items():
             key = minute, url
             if key not in download.space:
@@ -28,8 +38,30 @@ def read():
                 data = util.eat(response)
                 open = util.open(data)
             else:
+                data = None
                 open = 'NA'
-            yield space, minute, open
+            
+            row = space, minute, open
+
+            if annotate:
+                week = int(minute.strftime('%W'))
+                week_remainder = minute.timestamp() % week
+
+                row += (week, week_remainder, data)
+
+            yield row
+
+def space_forecast_importer():
+    start = datetime.datetime(2015, 2, 8) # Read this from a database.
+    reader = read(annotate = True, start = start)
+
+    with shelve.open('/tmp/spaceapi') as db:
+        for space, minute, open, week, week_remainder, data in reader:
+            db[(space, week, week_remainder)] = data
+
+    with shelve.open('/tmp/chartdata') as db:
+        for space, minute, open, week, week_remainder, data in reader:
+            db[(space, week, week_remainder)] = open
 
 def emit():
     import csv
