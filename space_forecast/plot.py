e4ef1e120b2cc5aeb063f88ef76e1e651da1f147
Thomas Levine <_@thomaslevine.com>
2015-06-01 16:16:07 +0000

    -

--- space_forecast/plot.py
+++ space_forecast/plot.py
@@ -10,16 +10,30 @@ TEMPLATE_PATH.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'vi
 
 app = Bottle()
 
-@app.route('/')
+DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+HOURS = list(range(24))
+SPACES = {'Hackistan'}
+
+@app.route('/<space>/<day>/<hour:int>')
 @view('plot')
-def home():
+def home(space, day, hour):
+    if space not in SPACES:
+        abort(404, 'Bad space')
+    elif day not in DAYS:
+        abort(404, 'Bad day')
+    elif hour not in HOURS:
+        abort(404, 'Bad hour')
+
+    radius = 2
+    hours = range(hour - radius, hour + radius + 1)
+
     return {
-        'space': 'Hackistan',
+        'space': space,
         'weeks': [
             {
                 'date': '2015-07-01',
-                'observations': [{'open': 'open', 'mirror_href': '.'} for _ in range(72)],
+                'observations': [{'open': 'open', 'mirror_href': '.'} for _ in range(60)],
             },
         ] * 3,
-        'hours': range(7, 7 + 6),
+        'hours': hours,
     }
